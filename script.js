7dc3d93e8eb2ba81873ff7ff09ecf8d88c493275
MoPaMo <mpm1301@web.de>
2020-12-09 19:56:34 +0100

    added svg count

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-$(function(){
-    var s = Snap("#svg");
-    var bigCircle = s.circle(150, 150, 100);
+$(function () {
+  var s = Snap("#svg");
+  var objects = [
+    Snap.select("#h-1"),
+    Snap.select("#h-2"),
+    Snap.select("#h-3"),
+    Snap.select("#head"),
+    Snap.select("#b"),
+    Snap.select("#a-l"),
+    Snap.select("#a-r"),
+    Snap.select("#b-l"),
+    Snap.select("#b-r"),
+  ];
+  for (ob of objects) {
+    ob.animate({ opacity: 0 }, 500);
+  }
+  var errorCount = 0;
+  function die(){
+      alert("You died")
+  }
+  function rerror() {
+    errorCount++;
     
-})
+    if (errorCount > 8) {
+      die();
+    }
+    objects[errorCount-1].animate({ opacity: 1 }, 1000);
+  }
+  rerror();
+  $(document).click(function(){
+      rerror()
+  })
+});
